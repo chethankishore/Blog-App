78006545e5d8d3a66bed6e9d0d5074d0f9973377
chethankishore <chethankishorecheyy77@gmail.com>
2026-05-14 21:55:07 +0530

    final commit

--- Blog-app-main/BLOG-APP-BACKEND/server.js
+++ Blog-app-main/BLOG-APP-BACKEND/server.js
@@ -13,7 +13,7 @@ config(); //process.env
 //Create express application
 const app = exp();
 //use cors middleware
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","https://blog-app-bice-three-91.vercel.app/"], credentials: true }));
 //add body parser middleware
 app.use(exp.json());
 //add cookie parser middleware
